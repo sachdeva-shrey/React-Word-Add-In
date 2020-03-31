13b8dde762e820732f7f7251a64199af387bbf23
Shrey Sachdeva <shreysachdeva.2000@gmail.com>
2020-04-01 02:11:25 +0530

    content control fix
    Signed-off-by: Shrey Sachdeva <shreysachdeva.2000@gmail.com>

--- src/taskpane/components/Experience.js
+++ src/taskpane/components/Experience.js
@@ -24,8 +24,9 @@ class Experience extends React.Component {
     const { selectedOption } = this.state;
     return (
       <div>
-        <Select value={selectedOption} onChange={this.handleChange} options={options} />
-        <PrimaryButton className="btn" onClick={() => this.handleNext()}>
+        <p>Enclose words with delimiters to set content controls on them</p>
+        <Select className="input"  value={selectedOption} onChange={this.handleChange} options={options} />
+        <PrimaryButton className="btn-center" onClick={() => this.handleNext()}>
           Next
         </PrimaryButton>
         {this.state.render && <Container value={this.state.selectedOption.value} />}
@@ -39,30 +40,60 @@ class Container extends React.Component {
     super();
   }
 
-  handleSubmit = async () => {
+  handleSubmit = async (e) => {
+    e.preventDefault();
     return Word.run(async context => {
-      let searchResults = context.document.body.search("[[]*[]]", { matchWildCards: true }).context.insertContentControl();
-      context.load(searchResults, 'font');
-      context.load(searchResults, 'text');
+      const delimeter = this.props.value;
+      var searchResults;
+      if (delimeter === "[") {
+        searchResults = context.document.body.search("[[]*[]]", { matchWildCards: true });
+      } else if (delimeter === `"`) {
+        searchResults = context.document.body.search(`"*"`, { matchWildCards: true });
+      }
+      //   else { searchResults = context.document.body.search("{}*{}",{ matchWildCards: true });  }
+
+      context.load(searchResults, "font");
+      context.load(searchResults, "text");
       return context.sync().then(function() {
+        let keywords = [];
         console.log("Found count: " + searchResults.items.length);
         for (let i = 0; i < searchResults.items.length; i++) {
+          keywords.push(searchResults.items[i].text);
           searchResults.items[i].font.color = "orange";
           searchResults.items[i].font.bold = true;
-          console.log(searchResults.items[i].text)
-        //   let search = searchResults.items[i].text;
-        //   let searched = context.document.body.search(search, { matchWholeWord: true }).insertContentControl();
-        // }
-    }
+        }
+        Word.run(async context => {
+          for (let k = 0; k < keywords.length; k++) {
+            console.log(keywords[0]);
+            let results = context.document.body.search(keywords[k]);
+            results.load("font/bold");
+            let customerContentControls = context.document.contentControls.getByTag("Acceptance");
+            customerContentControls.load("text");
+            await context.sync();
+            if (customerContentControls.items.length === 0) {
+              for (var i = 0; i < results.items.length; i++) {
+                results.items[i].font.bold = true;
+                console.log(results.items[i]);
+                var cc = results.items[i].insertContentControl();
+                cc.tag = "Acceptance";
+                cc.title = "Variable " + i;
+              }
+            }
+          }
+          return context.sync();
+        });
+        console.log(keywords);
         return context.sync();
+      });
     });
-  });
-}
+  };
 
   render() {
     return (
       <div>
-        <PrimaryButton onClick={this.handleSubmit}>Show Variables</PrimaryButton>
+        <PrimaryButton className="btn-center" onClick={this.handleSubmit}>
+          Show Variables
+        </PrimaryButton>
       </div>
     );
   }
